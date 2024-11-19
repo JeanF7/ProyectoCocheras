f115a1abf35965144b409e386c5199de82bd39ce
RensexLaptop <cfratia21_1@unc.edu.pe>
2024-11-18 19:48:47 -0500

    Puse mi contraseño, no se olviden de cambiarla

---
+++ msvc-cliente/src/test/java/org/rensex/springcloud/msvc_cliente/MsvcClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rensex.springcloud.msvc_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
