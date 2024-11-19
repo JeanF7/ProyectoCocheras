f1defdd85c958f07c2c2c6a97f99c496cc67a12c
Joseph <jalvareza21_1@unc.edu.pe>
2024-11-18 19:49:39 -0500

    dcdewc

---
+++ msvc-alquiler/src/main/java/org/joseph/msvc_alquiler/MsvcAlquilerApplication.java
@@ -0,0 +1,13 @@
+package org.joseph.msvc_alquiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcAlquilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcAlquilerApplication.class, args);
+	}
+
+}
