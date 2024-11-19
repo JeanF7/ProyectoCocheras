1f5c8f79c9bbfed7a225849a630ec2ee10f4a0d8
RenatoSebastian <rlescanon21_1@unc.edu.pe>
2024-11-18 19:54:45 -0500

    uyguyguyguy

---
+++ msvc-espacio/src/main/java/org/renato/sprincloud/msvc/espacio/msvc_espacio/MsvcEspacioApplication.java
@@ -0,0 +1,13 @@
+package org.renato.sprincloud.msvc.espacio.msvc_espacio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcEspacioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcEspacioApplication.class, args);
+	}
+
+}
